a25f5ad60b3098557aa41d629ab804dd3e004313
Sweettreee <xhrlehgus1234@naver.com>
2025-11-01 18:15:51 +0900

    feat(model): Define Lotto and validate numbers

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -1,6 +1,8 @@
-package lotto;
+package lotto.model;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -11,10 +13,18 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
+        Set<Integer> numSet = new HashSet<>(numbers);
+
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
-    }
 
-    // TODO: 추가 기능 구현
+        if (!numbers.stream().allMatch(i -> i >= 1 && i <= 45)) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45의 범위여야 합니다.");
+        }
+
+        if (numSet.size() != numbers.size()) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 서로 중복되지 않아야 합니다.");
+        }
+    }
 }
